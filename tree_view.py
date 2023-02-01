f7f6e1b65ccac91304c0bf1c27b7fa508c6ff319
shaun <shaun.roach@gmail.com>
2023-01-31 18:54:59 -0700

    some initial html printing and testing

--- tree_view.py
+++ tree_view.py
@@ -8,3 +8,39 @@ class TreeView():
     def print_html(self):
         if( self.root == None ):
             return ""
+
+
+class HtmlNode():
+    def __init__(self):
+        self.tag = ""
+        self.inner_text = ""
+        self.attribute_pairs = {}
+        self.nodes = []
+
+    def set_tag(self, tag):
+        self.tag = tag
+
+    def set_inner_text(self, inner_text):
+        self.inner_text = inner_text
+
+    def set_attribute(self, key, value):
+        self.attribute_pairs[key] = value
+
+    def add_node(self, new_node):
+        self.nodes.append(new_node)
+
+    def get_attribute_pairs_string(self):
+        sorted_keys = sorted(self.attribute_pairs)
+        attribute_strings = " ".join(map(lambda x: f"{x}='{self.attribute_pairs[x]}'", sorted_keys))
+        return attribute_strings
+
+    def print_html(self):
+        attribute_string = self.get_attribute_pairs_string()
+        attribute_buffer = " " if len(attribute_string) > 0 else ""
+        start_tag = f"<{self.tag}{attribute_buffer}{attribute_string}>"
+        end_tag = f"</{self.tag}>"
+
+        node_strings = list(map(lambda x: x.print_html(), self.nodes))
+        node_text = "".join(node_strings)
+
+        return f"{start_tag}{self.inner_text}{node_text}{end_tag}"
--- tree_view_test.py
+++ tree_view_test.py
@@ -5,3 +5,26 @@ def test_tree_view():
     tv = tree_view.TreeView()
     out_string = tv.print_html()
     assert(out_string == "")
+
+    anode = tree_view.HtmlNode()
+    anode.set_tag("html")
+    test_1_out = anode.print_html()
+    assert(test_1_out == "<html></html>")
+
+    anode.set_attribute("version", 1)
+    test_2_out = anode.print_html()
+    assert(test_2_out == "<html version='1'></html>")
+
+    anode.set_inner_text("green")
+    test_3_out = anode.print_html()
+    assert(test_3_out == "<html version='1'>green</html>")
+
+    bnode = tree_view.HtmlNode()
+    bnode.set_tag("p")
+
+    test_4_out = bnode.print_html()
+    assert(test_4_out == "<p></p>")
+
+    anode.add_node(bnode)
+    test_5_out = anode.print_html()
+    assert(test_5_out == "<html version='1'>green<p></p></html>")
