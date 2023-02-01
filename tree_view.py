ff078178737e85d7deb0e5f2970a6e7a5b544f19
shaun <shaun.roach@gmail.com>
2023-02-01 08:13:21 -0700

    added some linebreak and indentation logic

--- tree_view.py
+++ tree_view.py
@@ -31,6 +31,11 @@ class HtmlNode():
         new_node.recalculate_indent_level(self.indent_level + 1)
         self.nodes.append(new_node)
 
+    '''
+    This method sets the indentation method of all nodes inside.
+    The children node's indentation is adjusted to the parent identation + 1
+    And this is done recursively
+    '''
     def recalculate_indent_level(self, new_level):
         self.indent_level = new_level
         map(lambda x: x.recalculate_indent_level(new_level+1), self.nodes)
@@ -40,13 +45,21 @@ class HtmlNode():
         attribute_strings = " ".join(map(lambda x: f"{x}='{self.attribute_pairs[x]}'", sorted_keys))
         return attribute_strings
 
-    def print_html(self):
+    def print_html(self, linebreak_characters = '\n', indent_characters="  "):
         attribute_string = self.get_attribute_pairs_string()
         attribute_buffer = " " if len(attribute_string) > 0 else ""
-        start_tag = f"<{self.tag}{attribute_buffer}{attribute_string}>"
-        end_tag = f"</{self.tag}>"
+        indent_string = indent_characters*self.indent_level
+        start_tag = f"{indent_string}<{self.tag}{attribute_buffer}{attribute_string}>"
+        end_tag = f"{indent_string}</{self.tag}>"
 
         node_strings = list(map(lambda x: x.print_html(), self.nodes))
-        node_text = "".join(node_strings)
+        node_text = linebreak_characters.join(node_strings)
+        if( len(node_text) > 0 ):
+            node_text = f"{node_text}{linebreak_characters}"
 
-        return f"{start_tag}{self.inner_text}{node_text}{end_tag}"
+        inner_text_string = linebreak_characters
+        text_indent_string = indent_string + indent_characters
+        if( len(self.inner_text) > 0 ):
+            inner_text_string = f"{linebreak_characters}{text_indent_string}{self.inner_text}{linebreak_characters}"
+
+        return f"{start_tag}{inner_text_string}{node_text}{end_tag}"
--- tree_view_test.py
+++ tree_view_test.py
@@ -9,25 +9,25 @@ def test_tree_view():
     anode = tree_view.HtmlNode()
     anode.set_tag("html")
     test_1_out = anode.print_html()
-    assert(test_1_out == "<html></html>")
+    assert(test_1_out == "<html>\n</html>")
 
     anode.set_attribute("version", 1)
     test_2_out = anode.print_html()
-    assert(test_2_out == "<html version='1'></html>")
+    assert(test_2_out == "<html version='1'>\n</html>")
 
     anode.set_inner_text("green")
     test_3_out = anode.print_html()
-    assert(test_3_out == "<html version='1'>green</html>")
+    assert(test_3_out == "<html version='1'>\n  green\n</html>")
 
     bnode = tree_view.HtmlNode()
     bnode.set_tag("p")
 
     test_4_out = bnode.print_html()
-    assert(test_4_out == "<p></p>")
+    assert(test_4_out == "<p>\n</p>")
 
     anode.add_node(bnode)
     test_5_out = anode.print_html()
-    assert(test_5_out == "<html version='1'>green<p></p></html>")
+    assert(test_5_out == "<html version='1'>\n  green\n  <p>\n  </p>\n</html>")
 
     assert(anode.indent_level == 0)
     assert(bnode.indent_level == 1)
